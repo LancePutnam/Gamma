bac7048de56f2a73e36eb27c8bcf4c06918247c1
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2008-11-15 23:55:22 +0000

    fixed tests makefile for linux

--- tests/config.mk
+++ tests/config.mk
@@ -12,7 +12,7 @@ LIBS = -L${PREFIX}/lib -L/usr/lib
 CFLAGS = -O3 -I. -I../include/ -I${PREFIX}/include -I/usr/include
 
 # Linux/BSD
-#LDFLAGS = ${LIBS} ../libgamma.a -lportaudio -lsndfile
+#LDFLAGS = ${LIBS} ../libgamma.a -lrt -lasound -ljack -lpthread -lportaudio -lsndfile
 
 # Mac OS
 LDFLAGS = ${LIBS} ../libgamma.a -lportaudio -lsndfile -framework AudioUnit -framework AudioToolbox -framework CoreAudio -framework Carbon
