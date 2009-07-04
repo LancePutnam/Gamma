87e3c127c3ba85c85c8427e0330476fa86059d94
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2009-07-04 08:08:02 +0000

    Added thread unit test

--- tests/testUnits.cpp
+++ tests/testUnits.cpp
@@ -3,8 +3,13 @@
 #include "Access.h"
 #include "Conversion.h"
 #include "Types.h"
+#include "Thread.h"
 
 using namespace gam;
+
+THREAD_FUNCTION(threadFunc){
+	*(int *)user = 1; return NULL;
+}
 
 int main(int argc, char* argv[]){
 
@@ -252,6 +257,12 @@ int main(int argc, char* argv[]){
 	#undef T
 
 
+	// Thread
+	{	int x=0;
+		Thread t(threadFunc, &x);
+		t.wait();
+		assert(x == 1);
+	}
 
 	return 0;
 }
