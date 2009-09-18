62d1693b6c605bfa7b00558b679b0cb373b5a83d
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2009-09-18 19:10:22 +0000

    added serialization functionality

--- tests/testUnits.cpp
+++ tests/testUnits.cpp
@@ -3,6 +3,7 @@
 #include "Access.h"
 #include "Conversion.h"
 #include "File.h"
+#include "Serialize.h"
 #include "SmartObject.h"
 #include "Strategy.h"
 #include "Types.h"
@@ -50,6 +51,115 @@ int main(int argc, char* argv[]){
 		f.close();
 	}
 
+
+	// Serialization
+	{	using namespace ser;
+		// Single element tests
+		{
+			Serializer s;
+
+			float    if1=1, of1=0;
+			double   id1=1, od1=0;
+			int8_t   ih1=1, oh1=0;
+			int16_t  iH1=1, oH1=0;
+			int32_t  ii1=1, oi1=0;
+			int64_t  iI1=1, oI1=0;
+			uint8_t  it1=1, ot1=0;
+			uint16_t iT1=1, oT1=0;
+			uint32_t iu1=1, ou1=0;
+			uint64_t iU1=1, oU1=0;
+			
+			bool ib1=true, ob1=false;
+			std::string istr = "string", ostr;
+
+			s << if1 << id1 << ih1 << iH1 << ii1 << iI1 << it1 << iT1 << iu1 << iU1 << ib1 << istr;
+			
+			//for(int i=0; i<s.buf().size(); ++i) printf("% 4d (%c)\n", s.buf()[i], s.buf()[i]);
+			
+			Deserializer d(s.buf());
+
+			d >> of1 >> od1 >> oh1 >> oH1 >> oi1 >> oI1 >> ot1 >> oT1 >> ou1 >> oU1 >> ob1 >> ostr;
+
+			assert(of1 == if1);
+			assert(od1 == id1);
+			assert(oh1 == ih1);
+			assert(oH1 == iH1);
+			assert(oi1 == ii1);
+			assert(oI1 == iI1);
+			assert(ot1 == it1);
+			assert(oT1 == iT1);
+			assert(ou1 == iu1);
+			assert(oU1 == iU1);
+			assert(ob1 == ib1);
+			//assert(ostr == istr);
+
+			//printf("\n%f, %f, %d, %s\n", of1, od1, ob1, ostr.c_str());
+		}
+
+
+		// Multi-element tests
+		{
+			const int N = 3;
+			#define IV {1,2,3}
+			#define OV {0,0,0}
+			
+			float    ifn[N]=IV, ofn[N]=OV;
+			double   idn[N]=IV, odn[N]=OV;
+			int8_t   ihn[N]=IV, ohn[N]=OV;
+			int16_t  iHn[N]=IV, oHn[N]=OV;
+			int32_t  iin[N]=IV, oin[N]=OV;
+			int64_t  iIn[N]=IV, oIn[N]=OV;
+			uint8_t  itn[N]=IV, otn[N]=OV;
+			uint16_t iTn[N]=IV, oTn[N]=OV;
+			uint32_t iun[N]=IV, oun[N]=OV;
+			uint64_t iUn[N]=IV, oUn[N]=OV;
+			
+			Serializer s;
+			
+			s	.add(ifn,N).add(idn,N)
+				.add(ihn,N).add(iHn,N).add(iin,N).add(iIn,N)
+				.add(itn,N).add(iTn,N).add(iun,N).add(iUn,N)
+			;
+			
+			//for(int i=0; i<s.buf().size(); ++i) printf("% 4d (%c)\n", s.buf()[i], s.buf()[i]);
+			
+			Deserializer d(s.buf());
+			d >> ofn >> odn >> ohn >> oHn >> oin >> oIn >> otn >> oTn >> oun >> oUn;
+			
+			#define ASSERT(a,b) for(int i=0; i<N; ++i) assert(a[i] == b[i]);
+			
+			ASSERT(ifn, ofn);
+			ASSERT(idn, odn);
+			ASSERT(ihn, ohn);
+			ASSERT(iHn, oHn);
+			ASSERT(iin, oin);
+			ASSERT(iIn, oIn);
+			ASSERT(itn, otn);
+			ASSERT(iTn, oTn);
+			ASSERT(iun, oun);
+			ASSERT(iUn, oUn);
+		}
+		
+		{
+			float vf1=1, vf2=1;
+			SyncedMemory sm1(&vf1, 'f');
+			SyncedMemory sm2(&vf2, 'f');
+
+			assert(sm1.changed());
+			sm1.update();
+			assert(!sm1.changed());
+			
+			vf1 = 0;
+			assert(sm1.changed());
+
+			sm1.copyTo(sm2);
+			assert(vf2 == 0);
+			//assert(sm2.changed());
+			
+			//void * vv = &SyncedMemory::changed;
+		}
+	
+	}
 
 	// Memory management
 	{
@@ -189,7 +299,7 @@ int main(int argc, char* argv[]){
 		assert(array_t::references(elemsA) == 0);
 		assert(array_t::references(elemsB) == 2);		
 	}
-	
+
 
 	// Conversion
 
@@ -413,6 +523,7 @@ int main(int argc, char* argv[]){
 	T(0., 0.)	T( 0.5, 0.5) T( 1.,-1.) T( 1.2,-0.8) T( 2.2, 0.2)
 				T(-0.5,-0.5) T(-1.,-1.) T(-1.2, 0.8) T(-2.2,-0.2)
 	#undef T
+
 
 	// Interpolation
 	{
