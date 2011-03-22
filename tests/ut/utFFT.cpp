a5b9a9b27a023892e608e32e98268c764dc23719
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2011-03-22 20:39:44 +0000

    RFFT: additional flag in forward/inverse methods to simplify usage with complex buffers

--- tests/ut/utFFT.cpp
+++ tests/ut/utFFT.cpp
@@ -22,4 +22,16 @@
 //	printf("\n");
 ////	for(int i=1; i<N-1; i+=2) printf("[%3d] % f % f\n", i, ar[i], ar[i+1]);
 //	for(int i=0; i<N; i+=2) printf("% f\n% f ", ar[i], ar[i+1]);
+
+//	{
+//		for(int i=0; i<N; ++i){
+//			//buf[i+1] = i?0:1;
+//			buf[i+1] = 1 + cos(float(i)/N * M_2PI) + (i&1?-1:1);
+//			//buf[i+1] = (i&1?-1:1);
+//		}
+//		fft.forward(buf, true, true);
+//		for(int i=0; i<N+2; i+=2) printf("%6.3f %6.3f\n", buf[i], buf[i+1]);
+//		fft.inverse(buf, true, true);
+//		for(int i=0; i<N; ++i) printf("%g\n", buf[i+1]);
+//	}
 }
