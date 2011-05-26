ff232f5d2a98934151fd9ddb229dd4873061516b
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2011-05-26 20:28:12 +0000

    strip dead code

--- Gamma/fil.h
+++ Gamma/fil.h
@@ -18,10 +18,10 @@ namespace fil{
 template <uint32_t N, class T>
 class FixedDelay{
 public:
-	#define DO for(uint32_t i=0; i<N; ++i)
+	#define IT for(uint32_t i=0; i<N; ++i)
 
 	/// @param[in] v	Initial value of elements
-	FixedDelay(const T& v=T()){ DO mVals[i]=v; }
+	FixedDelay(const T& v=T()){ IT mVals[i]=v; }
 
 	/// Set nth delayed element
 	T& operator[](uint32_t i){ return mVals[i]; }
@@ -40,7 +40,7 @@ public:
 	/// Returns delay size number of elements
 	uint32_t size() const { return N; }
 
-	#undef DO
+	#undef IT
 
 protected:
 	mutable T mVals[N];
@@ -98,29 +98,6 @@ private:
 	uint32_t mPeriod;
 	mutable T mVal;
 };
-
-
-
-//template <int N, class Tv, class Tc>
-//class FixedSeries{
-//public:
-//
-//protected:
-//	
-//};
-//
-//
-//template <int Ni, int No, class Tv, class Tp>
-//class LCCD{
-//public:
-//
-//private:
-//	Tv mi[Ni];
-//	Tp ma[Ni];
-//	Tv mo[No];
-//	Tp mb[No];
-//};
-
 
 
 /// Integrates input with previous output
@@ -333,71 +310,6 @@ protected:
 	double mGain;
 };
 
-
-
-
-// Direct domain lowpass filter
-//template <class T=gam::real>
-//class Lowpass{
-//public:
-//	Lowpass(const T& cen=T(0), const T& wid=T(0.1), const T& peak=T(0))
-//	:	mCenter(cen), mPeak(peak)
-//	{ width(wid); }
-//
-//	T operator()(const T& x) const {
-//		const T c = x - mCenter;
-//		return num(c)/den(c)*0.5+0.5;
-//	}
-//	
-////	T den(const T& x) const { return mWidth+scl::abs(x); }
-////	T num(const T& x) const { return mPeak*mWidth-x; }
-//
-//	T den(const T& x) const { return mWidth+x*x; }
-//	T num(const T& x) const { return mPeak*mWidth-x*scl::abs(x); }
-//
-//	void width(const T& v){
-//		static const T eps = 0.000001;	// width must be greater than zero
-//		mWidth = scl::abs(v);
-//		if(mWidth < eps) mWidth = eps;
-//	}
-//
-//	void set(const T& cen, const T& wid, const T& peak){
-//		mCenter=cen; width(wid); mPeak=peak;
-//	}
-//
-//protected:
-//	T mCenter, mWidth, mPeak;
-//};
-//
-//
-//// Direct domain inverse comb filter
-//template <class T=gam::real>
-//class CombI{
-//public:
-//
-//	CombI(const T& frq=T(1), const T& resn=T(0.9), const T& phs=T(0))
-//	:	mFreq(frq), mPhase(phs)
-//	{ res(resn); }
-//
-//	T operator()(const T& x) const { return num(x)/den(x); }
-//	T den(const T& x) const { return T(1) + mRes*(mRes - T(2)*cos(M_2PI*(mFreq*x + mPhase))); }
-//	T num(const T& x) const { return mNum; }
-//
-//	void res(const T& v){
-//		static const T eps = 0.999999;	// width must be greater than zero
-//		mRes = scl::abs(v);
-//		if(mRes>eps) mRes=eps;
-//		mNum = T(1) + mRes*(mRes - T(2));
-//	}
-//	
-//	void set(const T& frq, const T& resn, const T& phs){
-//		mFreq=frq; res(resn); mPhase=phs;
-//	}
-//	
-//protected:
-//	T mFreq, mRes, mPhase;
-//	T mNum;
-//};
 
 } // fil::
 } // gam::
