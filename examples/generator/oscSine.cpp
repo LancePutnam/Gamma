934899cf20f356bacba8d19df124729ced2171c8
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2011-09-26 20:13:25 +0000

    sine example

--- examples/generator/oscSine.cpp
+++ examples/generator/oscSine.cpp
@@ -1,3 +1,10 @@
+/*	Gamma - Generic processing library
+	See COPYRIGHT file for authors and license information
+	
+	Example:		Generator / Oscillator / Sine
+	Description:	Plays a sinusoid
+*/
+
 #include <stdio.h>
 #include "Gamma/AudioIO.h"
 #include "Gamma/Oscillator.h"
@@ -5,17 +12,17 @@
 using namespace gam;
 
 Sine<> osc(440);
-//SineR<> osc(440);
-//SineD<> osc(440, 1, 10);
 
 void audioCB(AudioIOData& io){
 	while(io()){
-		io.out(0) = io.out(1) = osc() * 0.2;
+		float s = osc() * 0.2;
+		io.out(0) = s;
+		io.out(1) = s;
 	}
 }
 
-int main(int argc, char* argv[]){
-	AudioIO io(256, 44100., audioCB, NULL, 2);
+int main(){
+	AudioIO io(256, 44100., audioCB);
 	Sync::master().spu(io.framesPerSecond());
 	io.start();
 	printf("\nPress 'enter' to quit...\n"); getchar();
