761b45524bda7d62526b695696148efcf619c476
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2011-11-28 21:06:39 +0000

    update generators unit test

--- tests/ut/utGenerators.cpp
+++ tests/ut/utGenerators.cpp
@@ -4,6 +4,34 @@
 		{ Player<> p; Player<> q(p); }
 		{ Array<float> a; Player<> p(a, 1); }
 		//{ Player<> p("path/to/soundfile.wav"); }
+		
+		{
+			const int N = 128;
+			const double SR = 44100;
+
+			Array<float> a(N);
+			Player<float, ipl::Trunc, tap::Clip> p(a, SR);
+			
+			Sync::master().spu(SR);
+			
+			assert(p.min() == 0);
+			assert(p.max() == N);
+			assert(p.rate() == 1);
+			assert(p.sampleRate() == SR);
+			assert(p.channels() == 1);
+			
+			assert(p.pos() == 0);
+			
+			p.advance();
+			assert(p.pos() == 1);
+			
+			for(int i=0; i<N; ++i) p.advance();
+
+			assert(p.pos() == N);			
+			
+			p.reset();
+			assert(p.pos() == 0);
+		}
 	}
 }
 
