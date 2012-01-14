c7e17b06ced503b6302ae7acbf2572192cbaf364
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2012-01-14 01:43:11 +0000

    remove DataFile class

--- src/File.cpp
+++ src/File.cpp
@@ -1,9 +1,5 @@
 #include "Gamma/File.h"
 
-#define ULONG unsigned long
-#define UI4 unsigned long
-#define UI8 unsigned long long
-
 namespace gam{
 
 File::File(const char * path, const char * mode, bool open_)
@@ -65,80 +61,4 @@ uint32_t File::write(const char * path, const void * v, int size, int items){
 
 bool File::exists(const char * path){ File f(path, "r"); return f.open(); }
 
-
-
-
-DataFile::DataFile(const char * path)
-	: mPath(path), fp(0)
-{
-}
-
-DataFile::~DataFile(){
-	close();
-}
-
-void DataFile::close(){
-	if(fp){
-		fclose(fp);
-		fp = 0;
-	}
-}
-
-bool DataFile::openWrite(){
-	if(0 == fp){
-		fp = fopen(mPath, "wb");
-		if(fp) return true;
-	}
-	return false;
-}
-
-bool DataFile::openRead(){
-	if(0 == fp){
-		fp = fopen(mPath, "rb");
-		if(fp) return true;
-	}
-	return false;
-}
-
-int DataFile::read4(void * element){
-	return fscanf(fp, formatString<float >(), element);
-}
-
-int DataFile::read8(void * element){
-	return fscanf(fp, formatString<double>(), element);
-}
-
-void DataFile::read4(void * dst, ULONG len){
-	UI4 * d = (UI4 *)dst;
-	for(ULONG i=0; i<len; i++) read4(d + i);
-}
-
-void DataFile::read8(void * dst, ULONG len){
-	UI8 * d = (UI8 *)dst;
-	for(ULONG i=0; i<len; i++) read8(d + i);
-}
-
-void DataFile::write4(const void * element){
-	fprintf(fp, formatString<float >(), *(UI4 *)(element));
-}
-
-void DataFile::write8(const void * element){
-	fprintf(fp, formatString<double>(), *(UI8 *)(element));
-}
-
-void DataFile::write4(const void * src, ULONG len){
-	UI4 * s = (UI4 *)src;
-	for(ULONG i=0; i<len; i++) write4(s + i);
-}
-
-void DataFile::write8(const void * src, ULONG len){
-	UI8 * s = (UI8 *)src;
-	for(ULONG i=0; i<len; i++) write8(s + i);
-}
-
 } // gam::
-
-#undef ULONG
-#undef UI4
-#undef UI8
-
