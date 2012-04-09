845ee2cbeb0a1c98b17b956db651f9d6164aae36
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2012-04-09 18:54:09 +0000

    deBruijn: move data into function

--- Gamma/scl.h
+++ Gamma/scl.h
@@ -515,12 +515,14 @@ TEM T welch(T nphase);					///< Welch window function. nphase => [-1, 1)
 // internal
 namespace{
 
-	const uint32_t deBruijnBitPosition[32] = {
-		 0,  1, 28,  2, 29, 14, 24,  3, 30, 22, 20, 15, 25, 17,  4,  8,
-		31, 27, 13, 23, 21, 19, 16,  7, 26, 12, 18,  6, 11,  5, 10,  9
-	};
-
 	inline uint32_t deBruijn(uint32_t v){
+
+		static const uint32_t deBruijnBitPosition[32] = {
+			 0,  1, 28,  2, 29, 14, 24,  3, 30, 22, 20, 15, 25, 17,  4,  8,
+			31, 27, 13, 23, 21, 19, 16,  7, 26, 12, 18,  6, 11,  5, 10,  9
+		};
+
+		// Note: this is basically a hash function
 		return deBruijnBitPosition[(uint32_t(v * 0x077CB531UL)) >> 27];
 	}
 
