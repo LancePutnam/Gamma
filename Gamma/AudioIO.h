e0c22c1534c90a5cf5780a8edf445326d2439d88
l.putnam <l.putnam@520a9659-39ea-45c7-a602-f8490d8932a7>
2012-04-13 00:21:38 +0000

    AudioDevice ctor fix

--- Gamma/AudioIO.h
+++ Gamma/AudioIO.h
@@ -35,7 +35,7 @@ public:
 	
 	/// @param[in] nameKeyword	Keyword to search for in device name
 	/// @param[in] stream		Whether to search for input and/or output devices
-	AudioDevice(const std::string& nameKeyword, StreamMode stream = INPUT | OUTPUT);
+	AudioDevice(const std::string& nameKeyword, StreamMode stream = StreamMode(INPUT | OUTPUT));
 
 	~AudioDevice();
 
