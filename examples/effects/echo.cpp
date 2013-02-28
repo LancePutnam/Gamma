f5e7da6aa8f6230ed465525ee5b7333e4e5f50b5
Lance Putnam <putnam.lance@gmail.com>
2013-02-28 23:34:46 +0100

    add echo example

---
+++ examples/effects/echo.cpp
@@ -0,0 +1,30 @@
+/*	Gamma - Generic processing library
+	See COPYRIGHT file for authors and license information
+	
+	Example:		Effect / Echo
+	Description:	This demonstrates how to create an echo effect from a
+					delay line. An echo is simply a feedback delay line.
+*/
+
+#include "../examples.h"
+
+Delay<> delay(0.2);				// 200 ms delay
+Accum<> tmr(1./4);
+SineD<> src(1000, 0.1, 0.1);	// short sine plink
+
+void audioCB(AudioIOData& io){
+
+	while(io()){
+
+		if(tmr()) src.reset();
+
+		float s = src();
+
+		// Here is the feedback delay...
+		s += delay(s + delay()*0.7);
+	
+		io.out(0) = io.out(1) = s;
+	}
+}
+
+RUN_AUDIO_MAIN
