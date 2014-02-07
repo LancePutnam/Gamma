6cd23ea6f6b539ea59838ba552627c1556335c93
Lance Putnam <putnam.lance@gmail.com>
2014-02-07 20:02:54 +0100

    more comments

--- examples/generator/noise.cpp
+++ examples/generator/noise.cpp
@@ -2,17 +2,20 @@
 	See COPYRIGHT file for authors and license information
 	
 	Example:		Generator / Noise
-	Description:	The different colors of noise.
+	Description:	The different colors of noise. Each of the noise types has
+					a frequency-dependent power spectrum according to 
+					power(f) = 1/f^n. For white noise, n=0 resulting in a flat
+					spectrum. For pink and brown noise, n=1 and n=2, 
+					respectively producing progressively darker sounding noise.
 */
 
 #include "../examples.h"
 
-Accum<> tmr(1./2);
 NoiseWhite<> white;		// 1/f^0 noise
 NoisePink<> pink;		// 1/f^1 noise
 NoiseBrown<> brown;		// 1/f^2 noise
 int type = 2;			// Noise type
-
+Accum<> tmr(1./2);		// Timer for switching noise
 
 void audioCB(AudioIOData& io){
 
@@ -28,7 +31,10 @@ void audioCB(AudioIOData& io){
 			case 2: s = brown();	break;
 		}
 
-		io.sum(s*0.2, 0,1);
+		s *= 0.2;
+
+		io.out(0) = s;
+		io.out(1) = s;
 	}
 }
 
