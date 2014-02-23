94b04c7b58f017fa07cc7c3591685320975fc6c4
Lance Putnam <putnam.lance@gmail.com>
2014-02-23 13:16:08 +0100

    de Bruijn table use chars

--- Gamma/scl.h
+++ Gamma/scl.h
@@ -476,7 +476,7 @@ namespace{
 
 	inline uint32_t deBruijn(uint32_t v){
 
-		static const uint32_t deBruijnBitPosition[32] = {
+		static const unsigned char deBruijnBitPosition[32] = {
 			 0,  1, 28,  2, 29, 14, 24,  3, 30, 22, 20, 15, 25, 17,  4,  8,
 			31, 27, 13, 23, 21, 19, 16,  7, 26, 12, 18,  6, 11,  5, 10,  9
 		};
