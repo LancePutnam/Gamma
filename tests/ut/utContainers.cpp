c20043e69fa036bd8dfb1fb27ea220db9bd6153b
Lance Putnam <putnam.lance@gmail.com>
2014-06-10 13:02:47 +0200

    remove uint32_t

--- tests/ut/utContainers.cpp
+++ tests/ut/utContainers.cpp
@@ -32,7 +32,7 @@
 		array_t * a = new array_t(N, 123);
 		array_t * b = new array_t(*a);
 
-		for(uint32_t i=0; i<a->size(); ++i) assert((*a)[i] == 123);
+		for(unsigned i=0; i<a->size(); ++i) assert((*a)[i] == 123);
 		assert(a->elems() == b->elems());
 		assert(a->size() == b->size());
 		assert(array_t::references(a->elems()) == 2);
@@ -61,7 +61,7 @@
 		assert(a->elems() != b->elems());
 		assert(array_t::references(a->elems()) == 1);
 		assert(array_t::references(b->elems()) == 1);
-		for(uint32_t i=0; i<a->size(); ++i) assert((*b)[i] == 123);
+		for(unsigned i=0; i<a->size(); ++i) assert((*b)[i] == 123);
 		
 		t * elemsA = a->elems();
 		t * elemsB = b->elems();
