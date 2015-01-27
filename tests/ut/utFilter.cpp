7ac0671b3f91cfcae352cfec7556baa9170e38a9
LancePutnam <putnam.lance@gmail.com>
2015-01-27 10:51:18 +0100

    add MovingAvg test

--- tests/ut/utFilter.cpp
+++ tests/ut/utFilter.cpp
@@ -1,7 +1,16 @@
 {
-	{
-		Biquad<float, float, Domain1> f;
+{
+	Biquad<float, float, Domain1> fil;
 		
-	}
+}
+
+{
+	MovingAvg<> fil(4);
+	assert(near(fil(1), 0.25));
+	assert(near(fil(1), 0.5 ));
+	assert(near(fil(1), 0.75));
+	assert(near(fil(1), 1.  ));
+	assert(near(fil(1), 1.  ));
+}
 
 }
