9261581d33b85d18553683dbd8b7503cc0881d34
Lance Putnam <putnam.lance@gmail.com>
2021-04-20 16:32:09 +0100

    Complex: Add argUnit function

--- Gamma/Types.h
+++ Gamma/Types.h
@@ -131,6 +131,7 @@ public:
 	C operator / (const T& v) const { return C(*this) /= v; }
 	
 	T arg() const { return atan2(i, r); }					///< Returns argument (angle)
+	T argUnit() const { T r=arg()/(2*M_PI); return r>0 ? r : r+1; }	///< Return argument in unit interval [0, 1)
 	C conj() const { return C(r,-i); }						///< Returns conjugate, z*
 	T dot(const C& v) const { return r*v.r + i*v.i; }		///< Returns vector dot product
 	T norm() const { return sqrt(normSqr()); }				///< Returns norm (radius), |z|
