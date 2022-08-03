b794d76b44851515e7319791c639d8fdcc8c9e80
Lance Putnam <putnam.lance@gmail.com>
2022-08-03 13:43:27 +0100

    Create Ramped.h

---
+++ Gamma/Ramped.h
@@ -0,0 +1,58 @@
+#ifndef GAMMA_RAMPED_H_INC
+#define GAMMA_RAMPED_H_INC
+
+/*	Gamma - Generic processing library
+	See COPYRIGHT file for authors and license information
+
+	File Description: 
+	Value smoother
+*/
+
+namespace gam{
+
+/// Applies linear smoothing to value updates
+template <class T>
+class Ramped {
+public:
+
+	Ramped(){}
+	Ramped(T v){ *this = v; }
+
+	/// Set new target value
+
+	/// @param[in] v		new target value
+	/// @param[in] steps	number of samples to ramp over
+	Ramped& target(const T& v, float steps){
+		mTarget = v;
+		mInc = (mTarget - mVal)/steps;
+		return *this;
+	}
+
+	/// Get target value
+	const T& target() const { return mTarget; }
+
+	/// Set current and target values
+	Ramped& operator= (const T& v){ mVal=mTarget=v; return *this; }
+
+	/// Get smoothed value
+	operator T() const { return mVal; }
+
+	/// Update value; \returns smoothed value
+	T operator()(){
+		if(mVal != mTarget){
+			mVal += mInc;
+			if((mInc>0. && mVal>mTarget) || (mInc<0. && mVal<mTarget))
+				mVal = mTarget;
+		}
+		return mVal;
+	}
+
+private:
+	T mVal = T(0);
+	T mInc = T(0);
+	T mTarget = T(0);
+};
+
+} // gam::
+
+#endif // include guard
